63fd3e8f8def0aef0ae607ef2953ace5e87e97b9
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-04-16 18:07:23 +0530

    [#2] | adds userAunthentication handler | anjali/pradeep
    creates a handler for authenticating the user

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,27 @@
-import { Hono } from "hono";
+import { Context, Hono, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { Logger, ServeStatic } from "./types.ts";
 
-const createApp = (logger: Function, serveStatic: Function): Hono => {
+const authenticateUser = async (c: Context, next: Next) => {
+  const userID: string | undefined = getCookie(c, "user-ID");
+
+  if (!userID) {
+    return c.redirect("/login.html", 303);
+  }
+
+  await next();
+};
+
+const createApp = (logger: Logger, serveStatic: ServeStatic): Hono => {
   const app: Hono = new Hono();
-
   app.use(logger());
-  app.get("/", serveStatic({ root: "./public" }));
+
+  app.get("/login.html", serveStatic({ root: "./public" }));
+  app.get("/styles/login.css", serveStatic({ root: "./public" }));
+  app.get("/scripts/login.js", serveStatic({ root: "./public" }));
+
+  app.use("/*", authenticateUser);
+  app.get("/*", serveStatic({ root: "./public" }));
 
   return app;
 };
