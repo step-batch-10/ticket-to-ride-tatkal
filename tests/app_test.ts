40ce454a8c7b3484757bd6da4931892c9265abe9
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-04-16 18:02:01 +0530

    [#2] | Authentication tests | anjali/pradeep
    Adds tests for user authentication

--- tests/app_test.ts
+++ tests/app_test.ts
@@ -7,10 +7,19 @@ import { Context, Hono } from "hono";
 const logger = () => async (_: Context, n: Function) => await n();
 
 describe("App /", () => {
+  it("should redirect the user to login if user is not authenticated", async () => {
+    const app: Hono = createApp(logger, serveStatic);
+    const r: Response = await app.request("/");
+
+    assertEquals(r.status, 303);
+    assertEquals(r.headers.get("location"), "/login.html");
+    await r.text();
+  });
+
   it("should serve the home page for user, if user is authenticated", async () => {
     const app: Hono = createApp(logger, serveStatic);
     const r: Response = await app.request("/", {
-      headers: { cookie: "User-ID=1" },
+      headers: { cookie: "user-ID=1" },
     });
 
     assertEquals(r.status, 200);
