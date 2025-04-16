1534c40815912e089aee65a74c532c5abb27f73a
Dhanoj0810 <142652835+Dhanoj0810@users.noreply.github.com>
2025-04-16 17:39:43 +0530

    [#17] | Testing Player-Queue | Dhanoj/sarup
    Add test cases for WaitingQueue

---
+++ tests/player-queue_test.ts
@@ -0,0 +1,50 @@
+import { assertEquals } from "assert";
+import { describe, it } from "jsr:@std/testing/bdd";
+import { WaitingQueue } from "../src/models/player-queue.ts";
+
+describe("WaitingQueue", () => {
+  it("Should create waiting queue", () => {
+    const queue = new WaitingQueue();
+    assertEquals(queue.getWaitingQueue(), []);
+  });
+
+  it("Should add a player to waiting queue", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    assertEquals(queue.getWaitingQueue(), ["Dhanoj"]);
+  });
+
+  it("Should return true when waiting queue have 3 players", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    queue.add("sarup");
+    queue.add("hari");
+    assertEquals(queue.isFull(), true);
+  });
+
+  it("Should return false when waiting queue have less than 3 players", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    queue.add("sarup");
+    assertEquals(queue.isFull(), false);
+  });
+
+  it("Should return true when waiting queue have that player", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    assertEquals(queue.isPresent("Dhanoj"), true);
+  });
+
+  it("Should return false when waiting queue don't have that player", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    assertEquals(queue.isPresent("Sarup"), false);
+  });
+
+  it("Should clear the waiting queue", () => {
+    const queue = new WaitingQueue();
+    queue.add("Dhanoj");
+    queue.clear();
+    assertEquals(queue.getWaitingQueue(), []);
+  });
+});
