b7826850bda79bf09288c4ba1cc6796d0b0ba1a2
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-04-18 16:30:00 +0530

    [#30] | Adds fetchTickets | Anjali/Pradeep
    Adds fetchTickets

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -5,6 +5,66 @@ const renderMap = async () => {
   mapContainer.innerHTML = map.svg;
 };
 
+const cloneTemplate = (id) => {
+  const clone = document.querySelector(id);
+
+  return clone.content.cloneNode(true);
+};
+
+const highlightPick = (ticketTag) => {
+  const list = ticketTag.classList;
+
+  list.toggle("selected");
+};
+
+const insertTicketInfo = (parentTag, selector, data) => {
+  const tag = parentTag.querySelector(selector);
+  tag.innerText = data;
+};
+
+const generateTicket = (ticket) => {
+  const ticketTemplate = cloneTemplate("#ticket-template");
+  const ticketTag = ticketTemplate.querySelector(".ticket");
+
+  insertTicketInfo(ticketTemplate, ".from-station", ticket.from);
+  insertTicketInfo(ticketTemplate, ".to-station", ticket.to);
+  insertTicketInfo(ticketTemplate, ".points", ticket.points);
+
+  ticketTag.id = ticket.id;
+  ticketTag.addEventListener("click", () => highlightPick(ticketTag));
+
+  return ticketTag;
+};
+
+const displayTickets = (tickets) => {
+  const ticketsSelector = cloneTemplate("#ticket-selector-template");
+  const container = ticketsSelector.querySelector(".tickets-container");
+  const ticketTags = tickets.map(generateTicket);
+
+  container.append(...ticketTags);
+  document.querySelector(".tickets-display").replaceChildren(ticketsSelector);
+};
+
+const closePopUp = (threshold) => async () => {
+  const selectedTickets = document.querySelectorAll(".selected");
+  if (selectedTickets.length < threshold) return;
+  await fetch("/game/destination-tickets", { method: "POST" });
+  document.querySelector(".ticket-selection").remove();
+};
+
+const handleTicketsSelection = (destinationCards) => {
+  const { tickets, mininumPickup } = destinationCards;
+  displayTickets(tickets);
+  const chooseBtn = document.querySelector("#choose-tickets");
+  chooseBtn.addEventListener("click", closePopUp(mininumPickup));
+};
+
+const drawDestinationCards = async () => {
+  const res = await fetch("/game/destination-tickets");
+  const destinationCards = await res.json();
+  handleTicketsSelection(destinationCards);
+};
+
 const createFaceUpCard = (card) => {
   const div = document.createElement("div");
   div.classList.add("face-up-card");
@@ -20,12 +80,6 @@ const renderFaceupCards = async () => {
   const faceUpDivs = cards.map(createFaceUpCard);
   const faceupContainer = document.querySelector("#face-up-container");
   faceupContainer.append(...faceUpDivs);
-};
-
-const drawDestinationCards = async () => {
-  const res = await fetch("/game/destination-cards");
-  const cards = await res.json();
-  console.log(cards);
 };
 
 const renderPage = () => {
