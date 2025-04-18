3f0240c3e99efdb11611aa06672c3c0abc82c293
sushanthj674 <153906968+sushanthj674@users.noreply.github.com>
2025-04-18 12:07:15 +0530

    [#4] | adds color to face up cards  | sushanth/hari priya
    creates and add color to the face up card

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -5,18 +5,21 @@ const renderMap = async () => {
   mapContainer.innerHTML = map.svg;
 };
 
+const createFaceUpCard =(card) => {
+  const div = document.createElement("div");
+  div.classList.add("face-up-card");
+  div.style.backgroundColor = card.color;
+    div.innerText = card.color
+  return div;
+}
+
 const renderFaceupCards = async () => {
   const res = await fetch("/game/face-up-cards");
   const cards = await res.json();
-  console.log(cards);
 
+  const faceUpDivs = cards.map(createFaceUpCard);
   const faceupContainer = document.querySelector("#face-up-container");
-  cards.forEach((card) => {
-    const div = document.createElement("div");
-    div.classList.add("face-up-card");
-    div.style.backgroundColor = card.color;
-    faceupContainer.appendChild(div);
-  });
+faceupContainer.append(...faceUpDivs)
 };
 
 const drawDestinationCards = async () => {
