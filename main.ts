f6a06d1cca441fd1830bf6af4439beadf9e10df6
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-04-21 13:22:04 +0530

    [#14] | Changes the name | Dhanoj/Sarup/Hari Priya
    Changes the name GameHandler to GameManager

--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ import { createApp } from "./src/app.ts";
 import { logger } from "hono/logger";
 import { serveStatic } from "hono/deno";
 import { Users } from "./src/models/users.ts";
-import { GameHandler } from "./src/models/game-handlers.ts";
+import { GameManager } from "./src/models/game-handlers.ts";
 
 const main = (): void => {
   const args = {
     logger,
     serveStatic,
     users: new Users(),
-    gameHandler: new GameHandler(),
+    gameHandler: new GameManager(),
     reader: Deno.readTextFileSync,
   };
 
