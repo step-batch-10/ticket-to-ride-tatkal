73d1d9311c88fd6cf6f13f5000beee8dffb982e1
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2025-04-22 16:20:36 +0530

    adds setup files for git actions | devdatta/pradeep
    added pre-push, pre-rebase

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,11 @@
 chmod u+x hooks/pre-commit
 cp hooks/pre-commit .git/hooks/
 chmod u+x .git/hooks/pre-commit
+
+chmod u+x hooks/pre-rebase
+cp hooks/pre-rebase .git/hooks/
+chmod u+x .git/hooks/pre-rebase
+
+chmod u+x hooks/pre-push
+cp hooks/pre-push .git/hooks/
+chmod u+x .git/hooks/pre-push
