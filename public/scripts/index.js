ef4adb23ca9d67ffadf15e52d12974eb9079ca17
sushanthj674 <153906968+sushanthj674@users.noreply.github.com>
2025-04-22 12:56:52 +0530

    [#22] | Adds scripts for logout | Sushanth/Dhanoj
    Adds scripts for user logout

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,9 +3,17 @@ const joinLobby = async () => {
   globalThis.location = res.url;
 };
 
+const logOut = async () => {
+  const res = await fetch("/logout", { method: "DELETE" });
+  globalThis.location = res.url;
+};
+
 const main = () => {
   const playBtn = document.querySelector("#play-game-index");
   playBtn.addEventListener("click", joinLobby);
+
+  const logOutBtn = document.querySelector("#logoutButton");
+  logOutBtn.addEventListener("click", logOut);
 };
 
 globalThis.onload = main;
