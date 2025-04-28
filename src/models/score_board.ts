c2105be7f0c57f3c29ec93de6c8a11ea86c69256
SameeraBhanu <183719089+sameerabhanu@users.noreply.github.com>
2025-04-28 18:54:18 +0530

    [#42] | creates a scoreBoard class | sameera/ hari priya
    Calculates the count and total points of routes claimed

--- src/models/score_board.ts
+++ src/models/score_board.ts
@@ -1,22 +1,65 @@
 import { _ } from "https://cdn.skypack.dev/lodash";
 import { Player } from "./player.ts";
-import { PlayerScore } from "./schemas.ts";
+import { PlayerScore, Route, RouteScore } from "./schemas.ts";
 
 export class ScoreBoard {
   private players: Player[];
-  private playerScore: PlayerScore;
+  private routeScores: Map<number, number>;
 
   constructor(players: Player[]) {
     this.players = players;
-    this.playerScore = {
-      routeScores: [],
-    };
+    this.routeScores = new Map([
+      [1, 1],
+      [2, 2],
+      [3, 4],
+      [4, 7],
+      [5, 10],
+      [6, 15],
+    ]);
   }
 
-  // playerScorecard(player: Player) {
-  // }
+  private createRouteScore(distance: number, routeScores: RouteScore[]) {
+    const points = this.routeScores.get(distance)!;
+    const count = 1;
+    const totalPoints = points;
 
-  // populateScoreBoard() {
-  //  return this.players.map(this.playerScorecard);
-  // }
+    routeScores.push({
+      trackLength: distance,
+      points,
+      count,
+      totalPoints,
+    });
+  }
+
+  private updateRouteScore(routeScore: RouteScore) {
+    routeScore.count++;
+    routeScore.totalPoints += routeScore.points;
+  }
+
+  private getRouteScores(routes: Route[]) {
+    const routeScores: RouteScore[] = [];
+
+    routes.forEach(({ distance }) => {
+      const routeScore = _.find(routeScores, { trackLength: distance });
+
+      if (!routeScore) return this.createRouteScore(distance, routeScores);
+
+      this.updateRouteScore(routeScore);
+    });
+
+    return routeScores;
+  }
+
+  private playerScorecard(player: Player) {
+    const claimedRoutes = player.getClaimedRoutes();
+    const playerScore: PlayerScore = {
+      routeScores: this.getRouteScores(claimedRoutes),
+    };
+
+    return playerScore;
+  }
+
+  populateScoreBoard() {
+    return this.players.map((player) => this.playerScorecard(player));
+  }
 }
