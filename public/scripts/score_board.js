7bc999c48f4055ebc1facaf6871e2f62adcca850
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-04-29 15:33:52 +0530

    [#9] | Adds script for rendering scorecards | devdatta/sarup
    Rendering player scorecard - completed and incompleted tickets and route scores

--- public/scripts/score_board.js
+++ public/scripts/score_board.js
@@ -7,12 +7,12 @@ const cloneTemplate = (templateId) => {
   return clone;
 };
 
-const createTable = (player) => {
+const createTable = (scoreCard) => {
   const table = document.getElementById("table-body");
   const clone = cloneTemplate("score-card-template");
   const tr = clone.querySelector("tr");
   tr.setAttribute("data-bs-toggle", "offcanvas");
-  tr.setAttribute("data-bs-target", `#score-card-${player}`);
+  tr.setAttribute("data-bs-target", `#score-card-${scoreCard.playerName}`);
 
   table.append(clone);
 };
@@ -24,36 +24,91 @@ const styleOffcanvas = (offcanvas) => {
   offcanvas.style.left = "25%";
 };
 
-const setCanvas = (offcanvasClone, player) => {
-  const offcanvas = offcanvasClone.querySelector("div");
-  offcanvas.setAttribute("id", `score-card-${player}`);
-  styleOffcanvas(offcanvas);
-};
-
-const createTicket = (offcanvasClone, types, temp) => {
-  types.forEach((type) => {
-    const ticket = offcanvasClone.querySelector(type);
-    const clone = cloneTemplate(temp);
-    ticket.append(clone);
+const createRow = (data) => {
+  const tr = document.createElement("tr");
+  data.forEach((d) => {
+    const td = document.createElement("td");
+    td.innerText = d;
+    tr.append(td);
   });
+
+  return tr;
 };
 
-const createOffcanvas = (player) => {
+const createRouteTable = (routeScores, offcanvas) => {
+  const routeBody = offcanvas.getElementById("route-score-body");
+
+  routeScores.forEach(({ count, points, totalPoints, trackLength }) => {
+    const row = createRow([trackLength, points, count, totalPoints]);
+    routeBody.append(row);
+  });
+
+  return routeBody;
+};
+
+const setCanvas = (offcanvasClone, scoreCard) => {
+  const player = offcanvasClone.querySelector(".offcanvas-title");
+  player.innerText = scoreCard.playerName;
+  const offcanvasDiv = offcanvasClone.querySelector("div");
+  offcanvasDiv.setAttribute("id", `score-card-${scoreCard.playerName}`);
+  styleOffcanvas(offcanvasDiv);
+  const routeScores = scoreCard.routeScores;
+  createRouteTable(routeScores, offcanvasClone);
+};
+
+const createTicketBody = (tickets, ticketBody) => {
+  tickets.forEach(({ fromCity, toCity, points }) => {
+    const row = createRow([fromCity, toCity, points]);
+    ticketBody.append(row);
+  });
+
+  return ticketBody;
+};
+
+const createTicket = (offcanvasClone, type, temp, tickets) => {
+  const ticket = offcanvasClone.querySelector(type);
+  const clone = cloneTemplate(temp);
+  const heading = clone.querySelector(".heading");
+  heading.innerText = type.slice(1);
+  const ticketBody = clone.querySelector("#ticket-body");
+  createTicketBody(tickets, ticketBody);
+  ticket.append(clone);
+};
+
+const parse = (tickets, condition) => {
+  return tickets.filter(({ completed }) => completed === condition);
+};
+
+const createOffcanvas = (scoreCard) => {
   const offcanvasClone = cloneTemplate("offcanvas-template");
-  setCanvas(offcanvasClone, player);
+  setCanvas(offcanvasClone, scoreCard);
+  const completedTickets = parse(scoreCard.destinationTickets, true);
+  const incompletedTickets = parse(scoreCard.destinationTickets, false);
+
   createTicket(
     offcanvasClone,
-    [".completed-tickets", ".incompleted-tickets"],
+    ".completed-tickets",
     "ticket-template",
+    completedTickets,
   );
+
+  createTicket(
+    offcanvasClone,
+    ".incompleted-tickets",
+    "ticket-template",
+    incompletedTickets,
+  );
+
   document.querySelector("body").append(offcanvasClone);
 };
 
 const main = async () => {
   const scorecards = await fetchJSON("/game/scoreCards");
-  console.log(scorecards);
-  createTable("dhanoj");
-  createOffcanvas("dhanoj");
+
+  scorecards.forEach((s) => {
+    createTable(s);
+    createOffcanvas(s);
+  });
 };
 
 globalThis.onload = main;
